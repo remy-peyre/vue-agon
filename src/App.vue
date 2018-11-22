bf38af33c232dd51d594546e7d2981fd67c313d3
remy peyre <remy.peyre05@gmail.com>
2018-11-22 18:43:15 +0100

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FirstCompo msg="Le test mon Bro"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FirstCompo from './components/FirstCompo.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    FirstCompo
   }
 }
 </script>
