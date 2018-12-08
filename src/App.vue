df7647f0d7cc42788a508fa362996a16bf2ad241
remy peyre <remy.peyre05@gmail.com>
2018-12-08 18:41:09 +0100

    Deleted useless component test

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,17 @@
 <template>
   <div id="app">
-    <FirstCompo msg="Le test mon Bro"/>
     <LvlProgression variant="line-outline" :total=100 :current=45 />
     <LvlUp variant="line-outline" title='C le titre' text='C le text'/>
   </div>
 </template>
 
 <script>
-import FirstCompo from './components/FirstCompo.vue'
 import LvlProgression from './components/LvlProgression.vue'
 import LvlUp from './components/LvlUp.vue'
 
 export default {
   name: 'app',
   components: {
-    FirstCompo,
     LvlProgression,
     LvlUp
   }
